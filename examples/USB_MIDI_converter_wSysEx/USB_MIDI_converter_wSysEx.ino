8bd85e20c0c295cc4d7551a7701366f1a783ca15
Yuuichi Akagawa <Yuuichi.Akagawa@gmail.com>
2017-02-26 19:53:18 +0900

    Fix non UHS2.0 Shield workaround

--- examples/USB_MIDI_converter_wSysEx/USB_MIDI_converter_wSysEx.ino
+++ examples/USB_MIDI_converter_wSysEx/USB_MIDI_converter_wSysEx.ino
@@ -1,14 +1,29 @@
 /*
  *******************************************************************************
-   USB-MIDI to Legacy Serial MIDI converter with SysEx support
-   Copyright (C) 2017 Yuuichi Akagawa
-
-   Idea from LPK25 USB-MIDI to Serial MIDI converter
-     by Collin Cunningham - makezine.com, narbotic.com
-
-   This is sample program. Do not expect perfect behavior.
+ * USB-MIDI to Legacy Serial MIDI converter with SysEx support
+ * Copyright 2017 Yuuichi Akagawa
+ *
+ * Idea from LPK25 USB-MIDI to Serial MIDI converter
+ *   by Collin Cunningham - makezine.com, narbotic.com
+ *
+ * for use with USB Host Shield 2.0 from Circuitsathome.com
+ * https://github.com/felis/USB_Host_Shield_2.0
  *******************************************************************************
-*/
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *******************************************************************************
+ */
+
 
 #include <usbh_midi.h>
 #include <usbhub.h>
@@ -40,6 +55,10 @@ void doDelay(uint32_t t1, uint32_t t2, uint32_t delayTime);
 void setup()
 {
   _MIDI_SERIAL_PORT.begin(31250);
+
+  //Workaround for non UHS2.0 Shield
+  pinMode(7, OUTPUT);
+  digitalWrite(7, HIGH);
 
   if (Usb.Init() == -1) {
     while (1); //halt
